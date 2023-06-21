eb4e21ccb4d1f419a7f922a8a600713107effeac
spencerfar <spencer.farrell@dal.ca>
2023-06-20 23:19:12 -0400

    fixing unused models

--- latentvelo/models/__init__.py
+++ latentvelo/models/__init__.py
@@ -1,7 +1,4 @@
-from latentvelo.models.vae_model_test import VAETest
-from latentvelo.models.vae_model_latent_reg import VAELatentReg
 from latentvelo.models.vae_model import VAE
-from latentvelo.models.vae_model_splicing import VAESplicing
 from latentvelo.models.annot_vae_model import AnnotVAE
 from latentvelo.models.atac_reg_model import ATACRegModel
 from latentvelo.models.refine_model import RefineODE
